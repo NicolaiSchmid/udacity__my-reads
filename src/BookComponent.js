aae9a080bd00058d4414f3b2f1428f1ec8929a38
Nicolai Schmid <nicolai@schmid.uno>
2018-01-14 13:49:42 +0100

    Fix: Moved book link to cover image, so it doesn't trigger, when selecting the shelf

--- src/BookComponent.js
+++ src/BookComponent.js
@@ -13,28 +13,28 @@ export default class BookComponent extends Component {
         console.log(this.props.meta);
         return (
             <div className="book">
-                <a target="_blank" href={this.props.meta.infoLink}>
-                    <div className="book-top">
+                <div className="book-top">
+                    <a target="_blank" href={this.props.meta.infoLink}>
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.meta.imageLinks.thumbnail}")` }}></div>
-                        <div className="book-shelf-changer">
-                            <select defaultValue="moveTo" onInput={(event) => this.moveTo(event.target.value)}>
-                                <option value="moveTo" disabled>Move to...</option>
-                                {this.props.category !== 'currentlyReading' && (
-                                    <option value="currentlyReading">Currently Reading</option>
-                                )}
-                                {this.props.category !== 'wantToRead' && (
-                                    <option value="wantToRead">Want to Read</option>
-                                )}
-                                {this.props.category !== 'read' && (
-                                    <option value="read">Read</option>
-                                )}
-                                {this.props.category !== 'search' && (
-                                    <option value="none">None</option>
-                                )}
-                            </select>
-                        </div>
+                    </a>
+                    <div className="book-shelf-changer">
+                        <select defaultValue="moveTo" onInput={(event) => this.moveTo(event.target.value)}>
+                            <option value="moveTo" disabled>Move to...</option>
+                            {this.props.category !== 'currentlyReading' && (
+                                <option value="currentlyReading">Currently Reading</option>
+                            )}
+                            {this.props.category !== 'wantToRead' && (
+                                <option value="wantToRead">Want to Read</option>
+                            )}
+                            {this.props.category !== 'read' && (
+                                <option value="read">Read</option>
+                            )}
+                            {this.props.category !== 'search' && (
+                                <option value="none">None</option>
+                            )}
+                        </select>
                     </div>
-                </a>
+                </div>
                 <div className="book-title">{this.props.meta.title}</div>
 
                 {(this.props.meta.authors) && this.props.meta.authors.map((author, index) => {
